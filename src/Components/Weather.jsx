5deac59a13d828781c338d543aa4ca1c20d1374e
Nadia <nadia.m.amir@gmail.com>
2021-03-29 16:43:23 +0100

    display city and temp

--- src/Components/Weather.jsx
+++ src/Components/Weather.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
-export default function Weather(prop) {
-  console.log(prop.data)
+export default function Weather(props) {
   return (
     <div>
       <h1>{new Date().toLocaleString()}</h1>
-      <h1>London</h1>
+      <h1>{props.data.name}</h1>
+      <h2>{props.data.main.temp}</h2>
     </div>
   )
 }
