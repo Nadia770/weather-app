378db0d086a7f3bd9d9b584c4b96627ee8618b50
Nadia <nadia.m.amir@gmail.com>
2021-03-29 16:45:52 +0100

    Add subitForm function

--- src/App.js
+++ src/App.js
@@ -1,32 +1,46 @@
-import React, { Component } from 'react'
-import Form from './Components/form'
-import Weather from './Components/weather'
-const api_key = "bc6a57e2ee7c34c874ed55a5f4de1350"
+import React, { Component } from "react";
+import Form from "./Components/Form";
+import Weather from "./Components/Weather";
+const api_key = "bc6a57e2ee7c34c874ed55a5f4de1350";
 
 export class App extends Component {
+  state = {
+    weatherData: [],
+  };
 
-  state={
-    data :[]
+  componentDidMount() {
+    fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${api_key}`
+    )
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        this.setState({ weatherData: data });
+      });
   }
-  
 
-componentDidMount(){
-  fetch(`https://api.openweathermap.org/data/2.5/weather?q=London&appid=${api_key}`)
-  .then(response =>{
-   return response.json()})
-  .then(data =>{
-    this.setState(data)})
-}
+  submitForm=(city) =>{
+    fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}`
+    )
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data);
+        this.setState({ weatherData: data });
+      });
+  }
 
   render() {
     return (
       <div>
-        <Form/>
-        <Weather data={this.state.data}/>
+        <Form onSubmitForm={this.submitForm} />
+        <Weather data={this.state.weatherData} />
       </div>
-    )
+    );
   }
 }
 
-export default App
-
+export default App;
