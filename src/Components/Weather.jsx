54d11ffb2dca468724fc766e32408eb98b9d9057
Nadia <nadia.m.amir@gmail.com>
2021-03-30 09:09:45 +0100

    Add more styling

--- src/Components/Weather.jsx
+++ src/Components/Weather.jsx
@@ -5,11 +5,11 @@ export default function Weather(props) {
     <section>
       <div className="location-box">
         <h1 className="date">{new Date().toDateString()}</h1>
-        <h1 className="location">{props.data.name}</h1>
+        <h1 className="location">{props.data.name}, {props.data.sys.country}</h1>
       </div>
       <div className="weather-box">
-        {/* <h2nclassName="temp">{props.data.main.temp}</h2> */}
-        {/* <h2 className"weather">{props.data.weather[0].description}</h2> */}
+        <h2 className="temp">{props.data.main.temp}</h2>
+        <h2 className="weather">{props.data.weather[0].description}</h2>
       </div>
     </section>
   );
