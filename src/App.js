87c5595c5cc1c3e71031e2ef80073929e4c28fa2
Nadia <nadia.m.amir@gmail.com>
2021-03-30 16:44:01 +0100

    Handle invalid inputs

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
 import React, { Component } from "react";
 import Form from "./Components/Form";
 import Weather from "./Components/Weather";
-import LoadingIndicator from "./Components/LoadingIndicator"
+import LoadingIndicator from "./Components/LoadingIndicator";
 const api_key = "bc6a57e2ee7c34c874ed55a5f4de1350";
 
 export class App extends Component {
   state = {
     weatherData: [],
-    isLoading: true
+    isLoading: true,
   };
 
   componentDidMount() {
     fetch(
       `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${api_key}`
-      
     )
       .then((response) => {
         return response.json();
@@ -23,7 +22,7 @@ export class App extends Component {
       });
   }
 
-  submitForm=(city) =>{
+  submitForm = (city) => {
     fetch(
       `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}`
     )
@@ -32,24 +31,30 @@ export class App extends Component {
       })
       .then((data) => {
         console.log(data);
-        this.setState({ weatherData: data, isLoading: false  });
+        this.setState({ weatherData: data, isLoading: false });
       });
-  }
+  };
 
   render() {
-    const {isLoading, weatherData}  = this.state;
-    return ( 
-    <div>
-      {isLoading ? <LoadingIndicator/> : 
-      <div className={weatherData.main.temp > 293.15 ? "app-warm": "app"}>
-        <main>
-        <Form onSubmitForm={this.submitForm} />
-        <Weather data={weatherData} />
-        </main>
+    const { isLoading, weatherData } = this.state;
+    return (
+      <div>
+        {weatherData.cod === "404" ? (
+          <div className="container-error"> <p className="error">Error: Please refresh the page and type in a city.</p>
+          <LoadingIndicator />
+          </div>
+        ) : isLoading ? (
+          <LoadingIndicator />
+        ) : (
+          <div className={weatherData.main.temp > 293.15 ? "app-warm" : "app"}>
+            <main>
+              <Form onSubmitForm={this.submitForm} />
+              <Weather data={weatherData} />
+            </main>
+          </div>
+        )}
       </div>
-      }
-      </div>
-    )
+    );
   }
 }
 
