5e6265fc10504d08a0ae071169d8d54aa6377f5e
Nadia <nadia.m.amir@gmail.com>
2021-03-30 12:53:11 +0100

    Add Loading-Indicator Component

---
+++ src/Components/LoadingIndicator.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import Loader from "react-loader-spinner";
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
+
+export default function LoadingIndicator() {
+  return <Loader type="Hearts" color="#00BFFF" height={80} width={80} />;
+}
